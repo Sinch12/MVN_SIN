b4c684fc6df2fc5b7bfa26096a011646260d2d63
Sinchana K S <Sinchana K S@LAPTOP-8SEKENFG>
2024-10-24 12:30:22 +0530

    modify

--- src/test/java/com/actitime/testscript/customermodule.java
+++ src/test/java/com/actitime/testscript/customermodule.java
@@ -1,12 +1,16 @@
 package com.actitime.testscript;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 public class customermodule {
 @Test
 	public void createcustomer() {
-		Reporter.log("create customer", true);
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com");
+		driver.close();
 		}
 	}
 
